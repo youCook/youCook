7dadb13fec10638465c1b5958d6b22934835f9bb
Fer <fer@MacBook-Pro-de-Fer.local>
2019-09-26 12:14:25 +0200

    top 5

--- public/javascripts/jokes.js
+++ public/javascripts/jokes.js
@@ -1,4 +1,7 @@
+// import Axios from "axios";
+
 const apiHandler = new APIHandler();
+const topTenContainer = document.querySelector(".top-container");
 
 window.addEventListener("load", function(e) {
   e.preventDefault();
@@ -9,6 +12,29 @@ window.addEventListener("load", function(e) {
     document.querySelector("#randomjoke").innerHTML = '';
     document.querySelector("#randomjoke").innerHTML = `
     <q>${data.text}</q>
-    `
+    `;
+    axios.get("/post/index-top-ten")
+    .then(top => {
+      top.data.forEach(element => {
+      topTenContainer.innerHTML+=`
+        <li class="top-line">
+        ${element.likes} likes - <a href="/post/post-info/${element._id}">${element.postName}</a>
+        </li>
+      `;
+      })
+    })
   }));
+
+
+
+
+
+
+
+
+
+
+
+
+
 });
