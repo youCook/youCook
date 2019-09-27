0eed4894af3b0604d8987903c8bbee9868a74799
Pablo Giral Alonso <pablo@MacBook-Pro-de-Pablo.local>
2019-09-27 11:44:42 +0200

    css views

--- public/javascripts/randomrecipe.js
+++ public/javascripts/randomrecipe.js
@@ -1,16 +1,24 @@
-const apiHandler = new APIHandler();
+const apiHandler2 = new APIHandler();
 
 window.addEventListener("load", function(e) {
   e.preventDefault();
-  apiHandler.getRandomRecipe().then(res => {
+  apiHandler2.getRandomRecipe().then(res => {
     const { data } = res;
     console.log(data);
     document.querySelector("#randomrecipe").innerHTML = "";
     document.querySelector("#randomrecipe").innerHTML = `
-    <div><img src="${data.recipes[0].image}" alt="${data.recipes[0].title}"></div>
-    <div><h2>Recipe: ${data.recipes[0].title}<h2></div>
-    <div>Ready in ${data.recipes[0].readyInMinutes} minutes</div>
-    <div>Servings: ${data.recipes[0].servings}</div>
+    <div>
+      <img src="${data.recipes[0].image}" alt="${data.recipes[0].title}">
+    </div>
+    <div>
+      <h2>Recipe: ${data.recipes[0].title}<h2>
+    </div>
+    <div>
+      <p>Ready in ${data.recipes[0].readyInMinutes} minutes</p>
+    </div>
+    <div>
+      <p>Servings: ${data.recipes[0].servings}</p>
+    </div>
     `;
   });
 });
