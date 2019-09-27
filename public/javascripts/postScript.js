cebf58990b7d630d2daa301233b9d7715fa3a0af
Fer <fer@MacBook-Pro-de-Fer.local>
2019-09-27 13:47:18 +0200

    fixed post title feed

--- public/javascripts/postScript.js
+++ public/javascripts/postScript.js
@@ -10,7 +10,7 @@ window.onload = () => {
  
       <li>
         <div class="border-line">
-          <h2>Recipe Name: ${post.postName}</h2>
+          <h2>${post.postName}</h2>
           <img src="${post.picPath}" alt="recipe photo">
           <button class="post-edit" id="${post._id}">Edit post</button>
           <button class="post-delete" id="${post._id}">Delete post</button>
@@ -37,11 +37,11 @@ window.onload = () => {
         <input type="hidden" class="edit-id-link" value="${data.data._id}">
         <input type="submit" class="btn-send-post-edit" value="SAVE">
         </form>
-        <form action="/post/post-edit/update/${data.data._id}" method="post" enctype="multipart/form-data" class="input-form">
-          <label>Post photo</label>
-          <input type="file" name="picPath">
-          <input type="submit" value="SAVE">
-        </form>
+        // <form action="/post/post-edit/update/${data.data._id}" method="post" enctype="multipart/form-data" class="input-form">
+        //   <label>Post photo</label>
+        //   <input type="file" name="picPath">
+        //   <input type="submit" value="SAVE">
+        // </form>
       </li>
       `;
               document.querySelector(".btn-send-post-edit").onclick = function(
